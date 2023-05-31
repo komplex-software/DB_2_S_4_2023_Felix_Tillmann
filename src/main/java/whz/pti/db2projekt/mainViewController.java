e22ce6f9c5c961574305fcfa9697a281c0623955
Felix Schönfeld <felix.schoenfeld.00@gmail.com>
2023-05-31 11:04:05 +0200

    Mitarbeiter hinzugefügt

--- src/main/java/whz/pti/db2projekt/mainViewController.java
+++ src/main/java/whz/pti/db2projekt/mainViewController.java
@@ -2,9 +2,12 @@ package whz.pti.db2projekt;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import whz.pti.db2projekt.model.Mitarbeiter;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class mainViewController {
     @FXML
@@ -20,9 +23,31 @@ public class mainViewController {
         DBConnector dbConnector = new DBConnector();
         try {
             Connection connection = dbConnector.openConnection();
+
+            // Laden
+            loadMitarbeiter(connection);
+
             connection.close();
         } catch (SQLException e) {
             e.printStackTrace();
+        }
+    }
+    
+    private void loadMitarbeiter(Connection connection) throws SQLException {
+        Statement st = connection.createStatement();
+        String sql = "select * from  Mitarbeiter;";
+        ResultSet rs = st.executeQuery(sql);
+        while (rs.next()) {
+            Mitarbeiter newMitarbeiter = new Mitarbeiter(
+                    rs.getString("Vorname"),
+                    rs.getString("Nachname"),
+                    rs.getInt("Adresse_ID"),
+                    rs.getInt("Anrede_ID"),
+                    rs.getFloat("Lohn"),
+                    rs.getDate("Beschaeftigung"),
+                    rs.getBoolean("Verfuegbar")
+                    );
+            Mitarbeiter.addMitarbeiter(newMitarbeiter);
         }
     }
 }
